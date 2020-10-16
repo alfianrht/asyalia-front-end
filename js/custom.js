fd48295d23817825f1b3bf7a699321e90d34b714
alfianrht <alvianrht@gmail.com>
2020-10-16 19:36:36 +0700

    add scroll function

--- js/custom.js
+++ js/custom.js
@@ -13,9 +13,51 @@ wrap.on("scroll", function(e) {
 var header = $("#sliderFix");
   $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-       if (scroll >= 600) {
-          header.addClass("fixed-slider");
-        } else {
-          header.removeClass("fixed-slider");
-        }
+      if (scroll >= 600) {
+        $("#dress").css({"padding-top":"100px"});
+        header.addClass("fixed-slider");
+        $(".sub-footer").css({"margin-bottom":"195px"});
+
+      } else {
+        $("#dress").css({"padding-top":"150px"});
+        header.removeClass("fixed-slider");
+        // $("#dress").css({"padding-top":"0px"});
+      }
+});
+
+// Button Slider
+$("#buttonDress").click(function (){
+  $('html, body').stop(true, false).animate({
+      scrollTop: $("#dress").offset().top
+  }, 500);
+});
+
+
+
+// Navigasi Scroll
+
+$("#nextButton").click(function (){
+  $('html, body').stop(true, false).animate({
+      scrollTop: $("#dress").offset().top
+  }, 500);
+});
+
+var targetOffset = $("#dress").offset().top;
+
+var $w = $(window).scroll(function(){
+  
+    if ( $w.scrollTop() > targetOffset ) {   
+      $("#nextButton").click(function (){
+        $('html, body').stop(true, false).animate({
+            scrollTop: $("#dress").offset().top
+        }, 500);
+      });
+      // $("#nextButton").html('PILIH DRESS');
+    } else {
+      $("#nextButton").click(function (){
+        $('html, body').stop(true, false).animate({
+            scrollTop: $("#dress").offset().top
+        }, 500);
+      });
+    }
 });
